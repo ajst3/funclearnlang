ce25d47e0aee6dab54541ecf47c14ab7d0a2cea7
Austin Tercha <aut7@pitt.edu>
2020-12-10 15:39:08 -0500

    add test and makefile

---
+++ test/basiclexertest.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <compiler/lib/lexer.hpp>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  char *teststring = "name = 100\n";
+  return 0;
+}
