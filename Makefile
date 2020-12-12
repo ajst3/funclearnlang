e765fe35eddb869b20922eb84a0baedb876454df
Austin Tercha <aut7@pitt.edu>
2020-12-12 18:39:22 -0500

    fix makefiles and test code

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all: lexer
 	ar rcs libcompiler.a src/compiler/*o
 
 lexer:
-	$(Make) -C src/compiler
+	$(MAKE) -C src/compiler
 
 clean:
 	rm -f libcryptolib.a
--- test/basiclexertest.cpp
+++ test/basiclexertest.cpp
@@ -2,10 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 #include <compiler/lib/lexer.hpp>
+#include <compiler/lib/token.hpp>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  char *teststring = "name = 100\n";
+  char *teststring = "name = 100\nresult = func(name)\n";
+  printf("creating object\n");
+  Lexer lex = Lexer(teststring);
+  printf("tokenizing\n");
+  TokenList tokens = lex.tokenize();
+  printf("printing list\n");
+  tokens.printTokenList();
   return 0;
 }
